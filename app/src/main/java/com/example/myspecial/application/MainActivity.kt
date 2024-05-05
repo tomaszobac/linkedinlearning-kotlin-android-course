1b5cf0826e6da63028bc1694598d57bf76ae6402
Annyce Davis <annyce.davis@gmail.com>
2024-05-05 16:54:18 -0400

    [*]: Extending sample app Greeting composable

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.myspecial.application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
@@ -17,9 +21,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MySpecialApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(name = "Android")
                 }
             }
         }
@@ -28,16 +34,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
             text = "Hello $name!",
             modifier = modifier
-    )
+        )
+        Button(
+            onClick = { /* TODO */ }
+        ) {
+            Text(text = "Submit")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    device = "spec:width=720px,height=1080px,dpi=440"
+)
 @Composable
 fun GreetingPreview() {
     MySpecialApplicationTheme {
-        Greeting("Android")
+        Greeting(name = "Android")
     }
 }
