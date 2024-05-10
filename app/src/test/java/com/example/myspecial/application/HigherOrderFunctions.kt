5585187ef81038ae90d470ca3a60eeadd255ffdc
Annyce Davis <annyce.davis@gmail.com>
2024-05-10 17:11:49 -0400

    [-]: Removed the function return example

--- app/src/test/java/com/example/myspecial/application/HigherOrderFunctions.kt
+++ app/src/test/java/com/example/myspecial/application/HigherOrderFunctions.kt
@@ -7,32 +7,19 @@ fun operate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {
     return operation(x, y)
 }
 
-// makeMultiplier returns a function
-fun makeMultiplier(factor: Int): (Int) -> Int {
-    return { x -> x * factor }
-}
-
 class HigherOrderFunctionsTest {
 
     @Test
-    fun functionAsParameter() {
+    fun functionAsParameter1() {
         // the results of the operate function as addition
         val sum = operate(x = 5, y = 3, operation = { a, b -> a + b })
         println("Sum: $sum")
-
-        // the results of the operate function as multiplication
-        val product = operate(x = 5, y = 3) { a, b -> a * b }
-        println("Product: $product")
     }
 
     @Test
-    fun functionAsReturnValue() {
-        // the results of the makeMultiplier function as doubling function
-        val double = makeMultiplier(factor = 2)
-        val doubleResult = double(3)
-        println("Calling double function: $doubleResult")
-
-        // TODO: print the results of the makeMultiplier function as tripling function
-
+    fun functionAsParameter2() {
+        // the results of the operate function as multiplication
+        val product = operate(x = 5, y = 3) { a, b -> a * b }
+        println("Product: $product")
     }
 }
