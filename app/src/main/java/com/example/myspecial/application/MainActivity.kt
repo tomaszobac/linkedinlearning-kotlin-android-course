249709d78f6d626d0f303b6a1499d07566286193
Annyce Davis <annyce.davis@gmail.com>
2024-05-15 08:40:59 -0400

    [+]: Designs with composables end

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,6 +37,7 @@ fun Greeting(name: String, modifier: Modifier) {
     Column {
         Text(
             text = "Hello $name!",
+            style = MaterialTheme.typography.headlineLarge
         )
         Text(
             text = "Having fun?"
