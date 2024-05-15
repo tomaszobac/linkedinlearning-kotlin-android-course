30ce25c80d23b61c31a6c39c46c02728644e1939
Annyce Davis <annyce.davis@gmail.com>
2024-05-15 08:59:51 -0400

    [*]: Display ui elements end

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +37,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier) {
-    Column {
+    Box(
+        contentAlignment = Alignment.BottomEnd,
+        modifier = modifier.height(400.dp)
+    ) {
         Text(
             text = "Hello $name!",
             style = MaterialTheme.typography.headlineLarge
