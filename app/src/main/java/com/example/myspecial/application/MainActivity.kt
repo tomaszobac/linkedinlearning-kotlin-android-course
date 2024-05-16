f126cd293ee356c8c69776b28bb845bd22a90a7a
Annyce Davis <annyce.davis@gmail.com>
2024-05-16 11:56:05 -0400

    [+]: Use modifiers end

--- app/src/main/java/com/example/myspecial/application/MainActivity.kt
+++ app/src/main/java/com/example/myspecial/application/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,9 +12,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.ui.theme.MySpecialApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +38,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier) {
-    Column {
+    Column(
+        modifier = modifier.fillMaxSize()
+            .padding(16.dp)
+    ) {
         Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+                .clickable {  },
             text = "Hello $name!",
             style = MaterialTheme.typography.headlineLarge
         )
