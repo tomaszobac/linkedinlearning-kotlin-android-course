75712b81fcf444f39a0443fe536c4b131489843d
Annyce Davis <annyce.davis@gmail.com>
2024-05-29 13:22:32 -0400

    [-]: Removed dynamicColor mode

--- app/src/main/java/com/example/myspecial/application/ui/theme/OriginalTheme.kt
+++ app/src/main/java/com/example/myspecial/application/ui/theme/OriginalTheme.kt
@@ -41,7 +41,7 @@ private val OriginalLightColorScheme = lightColorScheme(
 fun MySpecialApplicationTheme(
         darkTheme: Boolean = isSystemInDarkTheme(),
         // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
+        dynamicColor: Boolean = false,
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
