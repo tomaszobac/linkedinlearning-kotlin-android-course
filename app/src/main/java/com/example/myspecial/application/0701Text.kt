12aa077920b8e768408d8de205260a8b799d3da4
Annyce Davis <annyce.davis@gmail.com>
2024-05-31 19:30:31 -0400

    [+]: String resources start

---
+++ app/src/main/java/com/example/myspecial/application/0701Text.kt
@@ -0,0 +1,43 @@
+package com.example.myspecial.application
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myspecial.application.ui.theme.AppTheme
+
+@Composable
+fun TextComposable(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "Hello, Learners!",
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_4_XL,
+    name = "Pixel 4 XL"
+)
+@Composable
+fun TextComposablePreview() {
+    AppTheme {
+        TextComposable()
+    }
+}
