afe4bedad0d0ee759f0820fc7d26752c9e8c55cc
Annyce Davis <annyce.davis@gmail.com>
2024-06-01 10:30:58 -0400

    [+]: Data entry start

---
+++ app/src/main/java/com/example/myspecial/application/lessons/0703Text.kt
@@ -0,0 +1,45 @@
+package com.example.myspecial.application.lessons
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myspecial.application.ui.theme.AppTheme
+
+@Composable
+fun TextEntryComposable(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.PIXEL_4_XL,
+    name = "Pixel 4 XL"
+)
+@Composable
+fun TextEntryComposablePreview() {
+    AppTheme {
+        TextEntryComposable()
+    }
+}
