820cd63b42dd36a3a075e322f91efc1e52351077
Annyce Davis <annyce.davis@gmail.com>
2024-06-01 09:36:07 -0400

    [+]: Use string resources end

--- app/src/main/java/com/example/myspecial/application/0701Text.kt
+++ app/src/main/java/com/example/myspecial/application/0701Text.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,7 +25,7 @@ fun TextComposable(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
-            text = "Hello, Learners!",
+            text = stringResource(R.string.hello_learners),
             style = MaterialTheme.typography.headlineLarge
         )
     }
