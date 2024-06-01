9276a3e57b091f009dbf1c7f8856526e1b37f2d1
Annyce Davis <annyce.davis@gmail.com>
2024-06-01 10:09:00 -0400

    [+]: Styled text end

--- app/src/main/java/com/example/myspecial/application/lessons/0702Text.kt
+++ app/src/main/java/com/example/myspecial/application/lessons/0702Text.kt
@@ -4,16 +4,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myspecial.application.R
 import com.example.myspecial.application.ui.theme.AppTheme
+import com.example.myspecial.application.ui.theme.Purple40
 
 @Composable
 fun StyledTextComposable(modifier: Modifier = Modifier) {
@@ -26,6 +31,16 @@ fun StyledTextComposable(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = stringResource(R.string.hello_learners),
+            color = Purple40,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Thin
+        )
+
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+            style = MaterialTheme.typography.headlineLarge,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
     }
 }
