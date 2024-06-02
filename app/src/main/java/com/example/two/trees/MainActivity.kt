67dd89b96662d91af67c6a332b872d2591dadbde
Annyce Davis <annyce.davis@gmail.com>
2024-06-02 18:06:53 -0400

    [*]: Introduced Two Trees App

---
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.two.trees
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.two.trees.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TwoTreesApp()
+        }
+    }
+}
+
+@Composable
+fun TwoTreesApp() {
+    AppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier.padding(innerPadding)
+            ) {
+
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    device = Devices.NEXUS_5,
+    name = "Nexus 5"
+)
+@Composable
+fun MyComposableNexusPreview() {
+    AppTheme {
+        TwoTreesApp()
+    }
+}
