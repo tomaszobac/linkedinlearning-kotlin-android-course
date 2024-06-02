c93222c4eca61560cfd181a41ca7bb5b32815b9e
Annyce Davis <annyce.davis@gmail.com>
2024-06-02 10:48:53 -0400

    [*]: Snackbar message end

--- app/src/main/java/com/example/myspecial/application/lessons/0705Text.kt
+++ app/src/main/java/com/example/myspecial/application/lessons/0705Text.kt
@@ -8,12 +8,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,9 +30,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.R
 import com.example.myspecial.application.ui.theme.AppTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
-fun SnackbarComposable(modifier: Modifier = Modifier) {
+fun SnackbarComposable(
+    modifier: Modifier = Modifier,
+    scope: CoroutineScope,
+    snackbarHostState: SnackbarHostState
+) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     val message = stringResource(R.string.message_text, email, password)
@@ -64,7 +75,17 @@ fun SnackbarComposable(modifier: Modifier = Modifier) {
         Button(
             onClick = {
                 controller?.hide()
-                Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                scope.launch {
+                    val result: SnackbarResult = snackbarHostState.showSnackbar(
+                        message = "I'm a Snackbar! 👋",
+                        actionLabel = "Show Info",
+                        duration = SnackbarDuration.Indefinite
+                    )
+
+                    if (result == SnackbarResult.ActionPerformed) {
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                    }
+                }
                 Log.i("SnackbarComposable", message)
             }
         ) {
@@ -82,11 +103,21 @@ fun SnackbarComposable(modifier: Modifier = Modifier) {
 )
 @Composable
 fun SnackbarComposablePreview() {
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
     AppTheme {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
+            snackbarHost = {
+                SnackbarHost(hostState = snackbarHostState)
+            }
         ) { padding ->
-            SnackbarComposable(modifier = Modifier.padding(padding))
+            SnackbarComposable(
+                modifier = Modifier.padding(padding),
+                scope = scope,
+                snackbarHostState = snackbarHostState
+            )
         }
     }
 }
