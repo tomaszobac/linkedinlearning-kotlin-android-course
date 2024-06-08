7f974a11fe79e02639381c30f765e16bebf54dcb
Annyce Davis <annyce.davis@gmail.com>
2024-06-08 19:13:33 -0400

    [*]: Load image resources at runtime

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -13,7 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -38,6 +43,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TwoTreesApp() {
+    var checked by remember { mutableStateOf(true) }
+    val imageId = if (checked) R.drawable.logo else R.drawable.olive_branch
+
     AppTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Column(
@@ -47,8 +55,15 @@ fun TwoTreesApp() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Switch(
+                    checked = checked,
+                    onCheckedChange = {
+                        checked = it
+                    }
+                )
+
                 Image(
-                    painter = painterResource(R.drawable.logo),
+                    painter = painterResource(imageId),
                     modifier = Modifier.size(256.dp)
                         .clip(CircleShape)
                         .background(Color.Green)
