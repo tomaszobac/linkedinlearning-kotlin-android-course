a26e22e64170f89c41dfb8f692b2812f02123acc
Annyce Davis <annyce.davis@gmail.com>
2024-06-08 20:07:24 -0400

    [+]: Vector drawable from svg start

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
@@ -21,13 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myspecial.application.R
 import com.example.two.trees.ui.theme.AppTheme
 
@@ -64,10 +59,7 @@ fun TwoTreesApp() {
 
                 Image(
                     painter = painterResource(imageId),
-                    modifier = Modifier.size(256.dp)
-                        .clip(CircleShape)
-                        .background(Color.Green)
-                        .padding(10.dp),
+                    modifier = Modifier.fillMaxWidth(),
                     contentScale = ContentScale.Fit,
                     contentDescription = null
                 )
