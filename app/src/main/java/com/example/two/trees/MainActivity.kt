1a179f046cebff4756ba40919f943659cd18ef22
Annyce Davis <annyce.davis@gmail.com>
2024-07-04 17:54:07 -0400

    [*]: Start of using viewmodel

--- app/src/main/java/com/example/two/trees/MainActivity.kt
+++ app/src/main/java/com/example/two/trees/MainActivity.kt
@@ -4,172 +4,80 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.myspecial.application.R
+import com.example.two.trees.ui.compose.TwoTreesAppBar
 import com.example.two.trees.ui.theme.AppTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StateApp()
+            TwoTreesApp()
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun StateApp() {
+fun TwoTreesApp() {
     AppTheme {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             topBar = {
-                TopAppBar(
-                    title = {
-                        Text("What state are you in?")
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primary,
-                        titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                        navigationIconContentColor = MaterialTheme.colorScheme.onPrimary
-                    )
-                )
+                TwoTreesAppBar()
             }
         ) { innerPadding ->
-            val states: List<String> = remember {
-                listOf(
-                    "Maryland",
-                    "Texas",
-                    "California",
-                    "Maine",
-                    "Utah",
-                    "Washington"
-                )
+            var productImageId by remember {
+                mutableIntStateOf(R.drawable.logo)
             }
-            var guess by remember { mutableStateOf("") }
-            var guessedState: String? by remember { mutableStateOf(null) }
 
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
+                modifier = Modifier.padding(innerPadding)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                EnterGuess(
-                    modifier = Modifier.padding(innerPadding),
-                    guess = guess,
-                    guessChanged = { newGuess ->
-                        guess = newGuess
-                    },
-                    checkGuess = {
-                        guessedState = states.find { it == guess }
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(productImageId)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        productImageId = generateNewImageId()
                     }
                 )
-
-                Spacer(modifier = Modifier.height(24.dp))
-
-                DisplayStates(states = states, guessedState = guessedState)
             }
         }
     }
 }
 
-@Composable
-fun EnterGuess(
-    modifier: Modifier = Modifier,
-    guess: String,
-    guessChanged: (String) -> Unit,
-    checkGuess: () -> Unit
-) {
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        TextField(
-            value = guess,
-            onValueChange = guessChanged,
-            placeholder = {
-                Text("Enter a state")
-            }
-        )
-
-        Spacer(modifier = Modifier.height(24.dp))
-
-        Button(
-            onClick = checkGuess
-        ) {
-            Text(
-                text = "Make your guess",
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-    }
-}
-
-@Composable
-fun DisplayStates(
-    modifier: Modifier = Modifier,
-    states: List<String>,
-    guessedState: String?
-) {
-    LazyVerticalGrid(
-        modifier = modifier,
-        columns = GridCells.Adaptive(minSize = 120.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(states) { item ->
-            val isSelected = guessedState == item
-            val borderColor =
-                if (isSelected) MaterialTheme.colorScheme.tertiary else Color.Transparent
-
-            Box(
-                modifier = Modifier
-                    .height(120.dp)
-                    .background(MaterialTheme.colorScheme.surfaceContainer)
-                    .border(
-                        width = 4.dp,
-                        color = borderColor
-                    ),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = if (isSelected) item else "",
-                    style = MaterialTheme.typography.labelLarge
-                )
-            }
-        }
+fun generateNewImageId(): Int {
+    return when(Random.nextInt(4)) {
+        0 -> R.drawable.delicate_olive_oil
+        1 -> R.drawable.lemon_olive_oil
+        2 -> R.drawable.chili_olive_oil
+        3 -> R.drawable.basil_olive_oil
+        else -> R.drawable.logo
     }
 }
 
@@ -181,6 +89,6 @@ fun DisplayStates(
 @Composable
 fun StateAppPreview() {
     AppTheme {
-        StateApp()
+        TwoTreesApp()
     }
 }
