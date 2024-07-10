9056f59763238d8474224c77c137ca4b59cdc618
Annyce Davis <annyce.davis@gmail.com>
2024-07-10 14:53:28 -0400

    [+]: Navigate between screens start

--- app/src/main/java/com/example/two/trees/ui/compose/HomeScreen.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/HomeScreen.kt
@@ -2,18 +2,24 @@ package com.example.two.trees.ui.compose
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,6 +54,28 @@ fun HomeScreen(
             modifier = Modifier.aspectRatio(1f / 1f),
             contentScale = ContentScale.Crop,
             contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun TakeATour(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.tertiary)
+    ) {
+        Text(
+            stringResource(id = R.string.take_a_tour).uppercase(),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(vertical = 16.dp, horizontal = 4.dp),
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.onTertiary,
+            textAlign = TextAlign.Center
         )
     }
 }
