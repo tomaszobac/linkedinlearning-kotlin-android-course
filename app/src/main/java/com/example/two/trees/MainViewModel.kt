4da0bbd5b0c1b820e503eb3dabac365c44c6be43
Annyce Davis <annyce.davis@gmail.com>
2024-07-13 19:28:19 -0400

    [*]: Kotlin data class end

--- app/src/main/java/com/example/two/trees/MainViewModel.kt
+++ app/src/main/java/com/example/two/trees/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.two.trees
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.two.trees.data.Product
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -13,7 +14,15 @@ class MainViewModel : ViewModel() {
     val quantity: StateFlow<Int> = _quantity
 
     init {
+        val product = Product(
+            name = "Sandy Sears",
+            imageFile = "efficiantur",
+            description = "molestiae",
+            size = 2659,
+            price = 2.3
+        )
         Log.i(TAG, "initialized")
+        Log.i(TAG, product.toString())
     }
 
 }
---
+++ app/src/main/java/com/example/two/trees/data/Product.kt
@@ -0,0 +1,9 @@
+package com.example.two.trees.data
+
+data class Product(
+    val name: String,
+    val imageFile: String,
+    val description: String,
+    val size: Int,
+    val price: Double
+)
