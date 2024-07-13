8a28013dabaed34a5fbf227d2633f0a9cfaf8177
Annyce Davis <annyce.davis@gmail.com>
2024-07-13 18:05:50 -0400

    [*]: Badge of honor solution

--- app/src/main/java/com/example/two/trees/ui/compose/TwoTreesBottomBar.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/TwoTreesBottomBar.kt
@@ -1,12 +1,16 @@
 package com.example.two.trees.ui.compose
 
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
@@ -20,19 +24,24 @@ fun TwoTreesBottomBar(
     modifier: Modifier = Modifier
 ) {
     NavigationBar(modifier = modifier) {
-        // TODO: create a variable that has the current quantity
+        val quantity by viewModel.quantity.collectAsStateWithLifecycle()
 
         screens.forEach { screen ->
             val label = stringResource(id = screen.labelResourceId)
             NavigationBarItem(
                 icon = {
-                    // TODO: update the icon to be a badged icon
-                    // TODO: only display badge when quantity not zero
-                    // TODO: and only on the shop screen
-                    Icon(
-                        imageVector = screen.icon,
-                        contentDescription = label
-                    )
+                    BadgedBox(
+                        badge = {
+                            if (quantity != 0 && screen.route == Screen.Shop.route) {
+                                Badge { Text(quantity.toString()) }
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = label
+                        )
+                    }
                 },
                 label = {
                     Text(label)
