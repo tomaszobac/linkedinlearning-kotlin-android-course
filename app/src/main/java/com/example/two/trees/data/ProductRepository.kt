d96ce98acc9f3dc139eced87fd7088fc9e8de23c
Annyce Davis <annyce.davis@gmail.com>
2024-07-14 16:51:33 -0400

    [+]: Retrofit interface end

--- app/src/main/java/com/example/two/trees/data/ProductRepository.kt
+++ app/src/main/java/com/example/two/trees/data/ProductRepository.kt
@@ -5,8 +5,15 @@ import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+interface ProductApi {
+    @GET("olive_oils_data.json")
+    suspend fun getProducts(): Response<List<Product>>
+}
 
 const val BASE_ENDPOINT_URL = "https://2873199.youcanlearnit.net/"
 
@@ -19,6 +26,10 @@ class ProductRepository(private val context: Context) {
             .baseUrl(BASE_ENDPOINT_URL)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
+    }
+
+    private val productApi: ProductApi by lazy {
+        retrofit.create(ProductApi::class.java)
     }
 
     fun getTextFromResources(resourceId: Int): String {
