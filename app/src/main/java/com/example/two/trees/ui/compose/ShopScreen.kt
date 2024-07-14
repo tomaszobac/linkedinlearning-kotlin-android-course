9bd83901a9366e49c74544924e291e00a69f974d
Annyce Davis <annyce.davis@gmail.com>
2024-07-14 14:43:21 -0400

    [*]: Share data with StateFlow start

--- app/src/main/java/com/example/two/trees/ui/compose/ShopScreen.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/ShopScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -36,6 +37,13 @@ fun ShopScreen(
         }
         item {
             FreeShipping()
+        }
+        item(
+            span = { GridItemSpan(currentLineSpan = 2) }
+        ) {
+            Text(
+                text = "",
+            )
         }
     }
 }
