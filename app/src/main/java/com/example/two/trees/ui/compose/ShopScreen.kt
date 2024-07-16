43e7854860b9368cbaa5bf904752a48f8b9407c2
Annyce Davis <annyce.davis@gmail.com>
2024-07-15 20:56:33 -0400

    [*]: Add lazygrid to composable end

--- app/src/main/java/com/example/two/trees/ui/compose/ShopScreen.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/ShopScreen.kt
@@ -2,10 +2,13 @@ package com.example.two.trees.ui.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,10 +27,21 @@ fun ShopScreen(
     products: List<Product>,
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    LazyVerticalGrid(
         modifier = modifier.fillMaxSize(),
+        columns = GridCells.Fixed(2)
     ) {
-        ShopLabel()
+        item {
+            ShopLabel()
+        }
+        item {
+            FreeShipping()
+        }
+        items(products) {
+            ElevatedCard {
+                Text(text = it.name)
+            }
+        }
     }
 }
 
