86c8922092abe59e27a959f45cf44757d44ae040
Annyce Davis <annyce.davis@gmail.com>
2024-07-18 14:29:06 -0400

    [*]: Save files to internal storage end

--- app/src/main/java/com/example/two/trees/data/ProductRepository.kt
+++ app/src/main/java/com/example/two/trees/data/ProductRepository.kt
@@ -2,11 +2,13 @@ package com.example.two.trees.data
 
 import android.content.Context
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import java.io.File
 
 interface ProductApi {
     @GET("olive_oils_with_images_data.json")
@@ -32,10 +34,21 @@ class ProductRepository(private val context: Context) {
         retrofit.create(ProductApi::class.java)
     }
 
+    private fun storeDataInFile(products: List<Product>) {
+        val listType = Types.newParameterizedType(List::class.java, Product::class.java)
+        val fileContents = moshi.adapter<List<Product>>(listType).toJson(products)
+
+        val file = File(context.cacheDir, "products.json")
+        file.writeText(fileContents, Charsets.UTF_8)
+    }
+
     suspend fun getProducts(): List<Product> {
         val response = productApi.getProducts()
-        return if (response.isSuccessful)
-            response.body().orEmpty()
+        return if (response.isSuccessful) {
+            val products = response.body()
+            products?.let{ storeDataInFile(it) }
+            products.orEmpty()
+        }
         else
             emptyList()
     }
