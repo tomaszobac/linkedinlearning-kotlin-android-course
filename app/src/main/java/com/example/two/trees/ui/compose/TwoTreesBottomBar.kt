133ca3be41f8b223145b0c7f8a298dab4b1c6ed2
tomaszobac <qwerts246@gmail.com>
2025-03-25 03:46:56 +0100

    solved

--- app/src/main/java/com/example/two/trees/ui/compose/TwoTreesBottomBar.kt
+++ app/src/main/java/com/example/two/trees/ui/compose/TwoTreesBottomBar.kt
@@ -1,12 +1,17 @@
 package com.example.two.trees.ui.compose
 
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
@@ -21,6 +26,7 @@ fun TwoTreesBottomBar(
 ) {
     NavigationBar(modifier = modifier) {
         // TODO: create a variable that has the current quantity
+        val quantity by viewModel.quantity.collectAsStateWithLifecycle()
 
         screens.forEach { screen ->
             val label = stringResource(id = screen.labelResourceId)
@@ -29,10 +35,28 @@ fun TwoTreesBottomBar(
                     // TODO: update the icon to be a badged icon
                     // TODO: only display badge when quantity not zero
                     // TODO: and only on the shop screen
-                    Icon(
-                        imageVector = screen.icon,
-                        contentDescription = label
-                    )
+                    BadgedBox(
+                        badge = {
+                            if (quantity > 0 &&
+                                currentDestination?.hierarchy?.any {
+                                    it.route == Screen.Shop.route
+                                } == true &&
+                                screen.icon == Screen.Shop.icon
+                            ) {
+                                Badge(
+                                    containerColor = Color.Red,
+                                    contentColor = Color.White
+                                ) {
+                                    Text("$quantity")
+                                }
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = label
+                        )
+                    }
                 },
                 label = {
                     Text(label)
